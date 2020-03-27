9cf5557f80bd96468a5c98e9453e52e132615fb9
LouisT123 <louistaing@aol.com>
2020-03-27 00:13:48 -0700

    Major Revisions grid 6x6

--- MyBattleship/MyBattleship.cpp
+++ MyBattleship/MyBattleship.cpp
@@ -1,5 +1,10 @@
-// MyBattleship.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ * Project: MyBattleship
+ * File: MyBattleship.cpp
+ * Author: Louis Taing
+ * Date: 03/27/2020
+ * Description: Main function
+ */
 
 #include <iostream>
 using namespace std;
@@ -9,19 +14,21 @@ void PlayGrid();
 
 int main()
 {
+    cout << "Custom Battleship by Louis Taing" << endl << endl;
+
     //intro 
     cout << "Welcome to Battleship Captain!\n" << endl;
     cout << "You will have three ships at your disposal" << endl;
-    cout << "The aircraft carrier (5 spaces), the Battleship (4 spaces) and the Submarine (3 Holes)" << endl << endl;
+    cout << "The aircraft carrier (5 spaces), the Battleship (4 spaces) and the Submarine (3 spaces)" << endl << endl;
     string temp;
     cout << "Press enter to continue" << endl;
     cin.ignore();
 
-    cout << endl << "The playfield is a 6 x 6 grid labeled a-f for the x axis and 1-6 for the y axis" << endl;
+    cout << endl << "The playfield is a 6 x 6 grid labeled by 2 digit numbers to represent the spaces" << endl;
    
     cout << "Rules:" << endl;
-    cout << "When placing a ship, the coordinates must be in consecutive order (either vertically or horizontally)" << endl;
-    cout << "For example (a4, a5, a6) or (a2, b2, c2) for the submarine)" << endl;
+    cout << "When placing a ship, the coordinates must adjacent to the previous coordinate's direction (either vertically or horizontally)" << endl;
+    cout << "For example ( 00 01 02 or 00 06 12 for the submarine)" << endl;
     cout << "vertical or horizontal ship placements only" << endl <<endl << endl;
 
     string temp2;
@@ -30,46 +37,48 @@ int main()
     //game startu
     //display grid
     PlayGrid();
-    //Aircraft carrier placement
+    //Aircraft carrier placement (CONCEPTUAL work in progress)
     cout << endl << "Ok! Where would you like to place your aircraft carrier? (5 spaces)" << endl;
-    cout << "Please type an intial coordinate according to key" << endl;
+  
     int carrierSpaces = 5;
     while (carrierSpaces > 0)
     {
-        string aircraftCarrier;
-        cin >> aircraftCarrier;
-        cout << "You have placed the part of the carrier at " + aircraftCarrier << endl;
-
-        //perform syntax check here (place into array after syntax check?)
-        carrierSpaces--;
+        cout << "Please type the intial coordinates according to key (ie 02 or 30)" << endl;
+        int aircraftCarrierPlacement;
+        cin >> aircraftCarrierPlacement;
+        if (aircraftCarrierPlacement >= 0 && aircraftCarrierPlacement < 36)
+        {
+            //Perform adjacent check here (function) (series all vert/all horiz as well as adjacent aka no snake ships)
+            cout << "You have placed the part of the carrier at " << aircraftCarrierPlacement << endl;
+            //fill play grid
+            carrierSpaces--;
+        }
+        else
+        {
+            cout << "valid integer in the range of 00 to 35 please" << endl;
+            //terminate temporary 
+            return 0;
+        }
+       
     }
    
-
-    //Battleship Placement
-    //Make sure you cannot assign duplicate
-    cout << "Ok! Where would you like to place your battleship? (4 spaces)" << endl;
-    cout << "Please type an intial coordinate according to key'" << endl;
-    int battleshipSpaces = 4;
-    while (battleshipSpaces > 0)
-    {
-        string battleship;
-        cin >> battleship;
-        cout << "You have placed part of the battleship at " + battleship << endl;
-        //perform syntax check here 
-        battleshipSpaces--;
-    }
- 
     
 }
 void PlayGrid()
 {
 
     cout << "Key: " << endl;
-    cout << " 0 | 1 | 2 " << endl;
-    cout << "-----------" << endl;
-    cout << " 3 | 4 | 5 " << endl;
-    cout << "-----------" << endl;
-    cout << " 6 | 7 | 8 " << endl;
+    cout << " 00 | 01 | 02 | 03 | 04 | 05 " << endl;
+    cout << "----------------------------" << endl;
+    cout << " 06 | 07 | 08 | 09 | 10 | 11 " << endl;
+    cout << "----------------------------" << endl;
+    cout << " 12 | 13 | 14 | 15 | 16 | 17 " << endl;
+    cout << "----------------------------" << endl;
+    cout << " 18 | 19 | 20 | 21 | 22 | 23 " << endl;
+    cout << "----------------------------" << endl;
+    cout << " 24 | 25 | 26 | 27 | 28 | 29 " << endl;
+    cout << "----------------------------" << endl;
+    cout << " 30 | 31 | 32 | 33 | 34 | 35 " << endl;
     cout << endl << endl;
 
 }
